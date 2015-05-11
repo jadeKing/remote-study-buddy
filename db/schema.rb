54334fbd3d40d42e764fa1b20b84b1500ef7bcd6
Joseph Knowles <joejknowles@gmail.com>
2015-05-11 14:33:17 +0100

    merged all conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150511111041) do
-=======
 ActiveRecord::Schema.define(version: 20150511125019) do
->>>>>>> origin/user_management
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "languages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
   end
 
-=======
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,5 +40,4 @@ ActiveRecord::Schema.define(version: 20150511125019) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> origin/user_management
 end
