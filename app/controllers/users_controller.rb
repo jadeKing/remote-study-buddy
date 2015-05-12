f25d37ebd2f2c47d8625f27f88a0ef8f1280a81a
Mark Pokorny <iridium193@gmail.com>
2015-05-12 11:27:32 +0100

    Added profile links to list of users studying given language.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def profile
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :user_languages
+  get '/users/:id/profile' => 'users#profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
